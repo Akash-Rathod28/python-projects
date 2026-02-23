cbd68d70b37181ab32473392f58acd2fdf589582
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-23 19:04:07 +0530

    Update computer_quiz_game.py

--- computer_quiz_game.py
+++ computer_quiz_game.py
@@ -4,27 +4,36 @@ playing = input("Do you want to play? ")
 if playing != 'yes'.lower():
     quit() # stops the program execution
 print("Okay Lets play :) ")
+score = 0
 
 answer = input("Waht does CPU stands for?")
 if answer == "central processing unit".lower():
     print("Correct!")
+    score += 1
 else:
     print("Incorect! ")
 
 answer = input("Who wrote constitutional assembly?")
 if answer == "dr br ambedkar".lower():
     print("Correct!")
+    score += 1
 else:
     print("Incorect! ")
 
 answer = input("Waht does GPU stands for?")
 if answer == "graphic processing unit".lower():
     print("Correct!")
+    score += 1
 else:
     print("Incorect! ")
 
 answer = input("Waht does RAM stands for?")
 if answer == "random access memory".lower():
     print("Correct!")
+    score += 1
 else:
     print("Incorect! ")
+
+print("You got" + str(score) + " questions correct!")
+print("You got" + str((score/4) * 100) + "%.")
+
